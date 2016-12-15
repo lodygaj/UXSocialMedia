febd2042d02605d1d3034a3e4c6ea5095cda7527
Joe Lodyga <jplodyga@eagle.fgcu.edu>
2016-12-15 16:00:40 -0500

    Added Create Story page and Thrifty branded fonts

--- app/src/main/java/com/gtoz/uxsocialmedia/CreateStoryFragment.java
+++ app/src/main/java/com/gtoz/uxsocialmedia/CreateStoryFragment.java
@@ -1,19 +1,31 @@
 package com.gtoz.uxsocialmedia;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.gtoz.uxsocialmedia.R.id.btnLogin;
 
 /**
  * Created by GtoZ on 12/13/2016.
  */
 
 public class CreateStoryFragment extends Fragment {
-    private TextView headerTxt, titleTxt, categoryTxt, locationTxt, captionTxt, uploadTxt;
+    private Button submitButton, cancelButton, uploadButton;
+    private TextView headerTxt, titleTxt, categoryTxt, locationTxt, captionTxt, uploadTxt, previewTxt;
+    private String selectedCategory;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -26,6 +38,11 @@ public class CreateStoryFragment extends Fragment {
         locationTxt = (TextView) view.findViewById(R.id.txtViewLocation);
         captionTxt = (TextView) view.findViewById(R.id.txtViewCaption);
         uploadTxt = (TextView) view.findViewById(R.id.txtViewUpload);
+        previewTxt = (TextView) view.findViewById(R.id.txtViewPreview);
+
+        submitButton = (Button) view.findViewById(R.id.submitButton);
+        cancelButton = (Button) view.findViewById(R.id.cancelButton);
+        uploadButton = (Button) view.findViewById(R.id.uploadButton);
 
         // Apply fonts
         Typeface font1 = Typeface.createFromAsset(getActivity().getAssets(), "fonts/ITCFranklinGothicStd-DmCpIt.otf");
@@ -36,12 +53,66 @@ public class CreateStoryFragment extends Fragment {
         locationTxt.setTypeface(font2);
         captionTxt.setTypeface(font2);
         uploadTxt.setTypeface(font2);
+        previewTxt.setTypeface(font2);
 
         // Setup category spinner
+        Spinner spinner = (Spinner) view.findViewById(R.id.categorySpinner);
+        // Create array of spinner elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Animal");
+        categories.add("Food");
+        categories.add("Music");
+        categories.add("Nature");
+        categories.add("Sport");
+        categories.add("Water");
 
+        selectedCategory = "Animal";
 
+        // Create spinner adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_item, categories);
+        // Set drop down layout style for spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Attach adapter to spinner
+        spinner.setAdapter(adapter);
+        // Listener to determine what happens when item is selected from drop down
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCategory = parent.getItemAtPosition(position).toString();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // Not Used
+            }
+        });
 
+        // Set up button click listeners
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+                startActivity(mainIntent);
+
+            }
+        });
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent uploadIntent = new Intent();
+                //uploadIntent.setType("image/*");
+                //uploadIntent.setAction(Intent.ACTION_GET_CONTENT);
+                //startActivityForResult(Intent.createChooser(uploadIntent, "Select image from gallery", 1));
+            }
+        });
 
         return view;
     }
