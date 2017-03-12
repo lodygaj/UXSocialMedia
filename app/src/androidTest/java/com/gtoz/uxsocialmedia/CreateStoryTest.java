6aaed18308df522f1ce5fc705495ebf8fa992769
brendansaage <bdsaage0308@eagle.fgcu.edu>
2017-03-12 14:35:55 -0400

    CreateStoryTest done.

--- app/src/androidTest/java/com/gtoz/uxsocialmedia/CreateStoryTest.java
+++ app/src/androidTest/java/com/gtoz/uxsocialmedia/CreateStoryTest.java
@@ -1,24 +1,37 @@
 package com.gtoz.uxsocialmedia;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 
 import org.hamcrest.Matcher;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.PendingIntent.getActivity;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by GtoZ on 3/9/2017.
@@ -32,10 +45,6 @@ public class CreateStoryTest {
     @Before
     public void setUp() {
         //DBHelper dbHelper = new DBHelper(InstrumentationRegistry.getInstrumentation().getTargetContext());
-    }
-
-    @Test
-    public void testLoadCreateStoryFragment() {
         // Locate Create Story tab
         Matcher<View> matcher = allOf(withText("Create"),
                 isDescendantOfA(withId(R.id.tabs)));
@@ -43,17 +52,45 @@ public class CreateStoryTest {
         onView(matcher).perform(click());
         // Verify that fragment has loaded by checking that title is displayed
         onView(withText("Create a Story")).check(matches(isCompletelyDisplayed()));
+
+        try {
+            Thread.sleep(9999);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-//    @Test
-//    public void newStoryTest() {
-//        // Fill in sample story
-//        //onView(withId(R.id.edtTxtTitle)).perform(typeText("New Story"), closeSoftKeyboard());
-//        //onView(withId(R.id.edtTxtLocation)).perform(typeText("Fort Myers"), closeSoftKeyboard());
-//        //onView(withId(R.id.edtTxtCaption)).perform(typeText("This is a new story."), closeSoftKeyboard());
-//        //onView(withId(R.id.submitButton)).perform(click());
-//
-//        //Assert.assertEquals();
-//    }
+    @Test
+    public void titleTextTest(){
+        onView(withId(R.id.edtTxtTitle)).perform(typeText("New Story"), closeSoftKeyboard());
+        onView(withId(R.id.edtTxtTitle)).check(matches(withText("New Story")));
+
+        onView(withId(R.id.categorySpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Sports"))).perform(click());
+        onView(withId(R.id.categorySpinner)).check(matches(withSpinnerText(containsString("Sports"))));
+
+        onView(withId(R.id.edtTxtLocation)).perform(typeText("Fort Myers"), closeSoftKeyboard());
+        onView(withId(R.id.edtTxtLocation)).check(matches(withText("Fort Myers")));
+
+
+        onView(withId(R.id.edtTxtCaption)).perform(typeText("This is a new story."), closeSoftKeyboard());
+        onView(withId(R.id.edtTxtCaption)).check(matches(withText("This is a new story.")));
+
+
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withText("Story Submitted"))
+                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.cancelButton)).perform(click());
+        onView(withText("THRIFTY STORIES")).check(matches(isCompletelyDisplayed()));
+    }
 
 }
+
